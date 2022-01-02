8e259812502b4fa3269e359e373b0bacefe13958
jhonboy121 <alfredmathew05@gmail.com>
2022-01-02 18:09:39 +0530

    SystemUI: Refactor notification counter
    * Update icons from NotificationIconAreaController when settings change and set initial state from StatusBarIconController
    * Also use value of config_statusBarShowNumber as the default
    
    @neobuddy89: Add required NPE checks to properly apply settings on boot.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarIconController.java
@@ -26,6 +26,7 @@ import static com.android.systemui.statusbar.phone.StatusBarIconHolder.TYPE_WIFI
 import android.annotation.Nullable;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.ArraySet;
@@ -405,6 +406,10 @@ public interface StatusBarIconController {
         private StatusBarIconController mController;
         private final StatusBarLocation mLocation;
 
+        private boolean mShowWifiStandard;
+        private static final String SHOW_WIFI_STANDARD_ICON =
+                "system:" + Settings.System.SHOW_WIFI_STANDARD_ICON;
+
         // Enables SystemUI demo mode to take effect in this group
         protected boolean mDemoable = true;
         private boolean mIsInDemoMode;
@@ -412,10 +417,7 @@ public interface StatusBarIconController {
 
         protected ArrayList<String> mBlockList = new ArrayList<>();
 
-        private boolean mShowWifiStandard;
-
-        private static final String SHOW_WIFI_STANDARD_ICON =
-                "system:" + Settings.System.SHOW_WIFI_STANDARD_ICON;
+        private final boolean mShowNotificationCount;
 
         public IconManager(
                 ViewGroup group,
@@ -431,6 +433,10 @@ public interface StatusBarIconController {
             mContext = group.getContext();
             mIconSize = mContext.getResources().getDimensionPixelSize(
                     com.android.internal.R.dimen.status_bar_icon_size);
+            mShowNotificationCount = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUSBAR_NOTIF_COUNT,
+                mContext.getResources().getBoolean(R.bool.config_statusBarShowNumber) ? 1 : 0,
+                UserHandle.USER_CURRENT) == 1;
             mLocation = location;
 
             if (statusBarPipelineFlags.runNewMobileIconsBackend()) {
@@ -525,6 +531,7 @@ public interface StatusBarIconController {
         protected StatusBarIconView addIcon(int index, String slot, boolean blocked,
                 StatusBarIcon icon) {
             StatusBarIconView view = onCreateStatusBarIconView(slot, blocked);
+            view.setShowCount(mShowNotificationCount);
             view.set(icon);
             mGroup.addView(view, index, onCreateLayoutParams());
             return view;
