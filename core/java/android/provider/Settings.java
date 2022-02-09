b2d83885ccb2bbfa98f39b6cb77499d8b282cd35
LibXZR <i@xzr.moe>
2022-02-09 09:58:48 +0800

    Add support for GMS switch
    @neobuddy89:
    * Adapted for A13 QPR1
    * Enabled by default
    * Updated package list
    * Inject same package list in ParallelSpaceManagerService
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -11300,6 +11300,12 @@ public final class Settings {
         public static final String DOZE_ON_CHARGE = "doze_on_charge";
 
         /**
+         * Control whether GMS is enabled for this user.
+         * @hide
+         */
+        public static final String GMS_ENABLED = "gms_enabled";
+
+        /**
          * These entries are considered common between the personal and the managed profile,
          * since the managed profile doesn't get to change them.
          */
