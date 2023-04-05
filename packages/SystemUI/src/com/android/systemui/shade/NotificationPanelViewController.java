27c1000a97ecdf9f9ba9b9dbf0527141e6481a74
Jake Weinstein <jake@aospa.co>
2023-04-05 13:19:31 +0800

    SystemUI: Forward port QS fling animation boost to NotificationPanelV…
    …iewController
    
    PanelViewController.java was deleted and migrated
    to NotificationPanelViewController.java in Android
    13 QPR2.
    
    Change-Id: I2113e059709da2154479d9a6a4a3d5100f829a52
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -86,6 +86,7 @@ import android.transition.Transition;
 import android.transition.TransitionManager;
 import android.transition.TransitionSet;
 import android.transition.TransitionValues;
+import android.util.BoostFramework;
 import android.util.IndentingPrintWriter;
 import android.util.Log;
 import android.util.MathUtils;
@@ -730,6 +731,11 @@ public final class NotificationPanelViewController implements Dumpable {
     private int mOccludedToLockscreenTransitionTranslationY;
     private boolean mUnocclusionTransitionFlagEnabled = false;
 
+    /**
+     * For PanelView fling perflock call
+     */
+    private BoostFramework mPerf = null;
+
     private final String[] mAppExceptions;
 
     /*Reticker*/
@@ -1042,6 +1048,7 @@ public final class NotificationPanelViewController implements Dumpable {
                     }
                 });
         dumpManager.registerDumpable(this);
+        mPerf = new BoostFramework();
     }
 
     private void unlockAnimationFinished() {
@@ -2168,6 +2175,10 @@ public final class NotificationPanelViewController implements Dumpable {
                 animator.setDuration(mFixedDuration);
             }
         }
+        if (mPerf != null) {
+            String currentPackage = mView.getContext().getPackageName();
+            mPerf.perfHint(BoostFramework.VENDOR_HINT_SCROLL_BOOST, currentPackage, -1, BoostFramework.Scroll.PANEL_VIEW);
+        }
         animator.addListener(new AnimatorListenerAdapter() {
             private boolean mCancelled;
 
@@ -2180,11 +2191,17 @@ public final class NotificationPanelViewController implements Dumpable {
 
             @Override
             public void onAnimationCancel(Animator animation) {
+                if (mPerf != null) {
+                    mPerf.perfLockRelease();
+                }
                 mCancelled = true;
             }
 
             @Override
             public void onAnimationEnd(Animator animation) {
+                if (mPerf != null) {
+                    mPerf.perfLockRelease();
+                }
                 if (shouldSpringBack && !mCancelled) {
                     // After the shade is flung open to an overscrolled state, spring back
                     // the shade by reducing section padding to 0.
